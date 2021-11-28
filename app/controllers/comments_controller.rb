36304aaa625e362bde602b0dba93e31331631a1a
msalerno93 <msalerno1993@gmail.com>
2021-11-28 17:12:18 -0500

    comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-    
+    before_action :set_workout
 
     def index
     end
@@ -9,6 +9,14 @@ class CommentsController < ApplicationController
     end
 
     def create
+        @comment = @workout.comments.build(text: comment_params[:text], user: @current_user)
+        if @comment.save
+            redirect_to user_workout_path(@current_user, @workout)
+        else
+            # redirect_to new_user_workout_comment_path(@current_user, @workout)
+            @errors = @comment.errors.full_messages
+            render :new
+        end
     end
 
     def edit
@@ -23,6 +31,10 @@ class CommentsController < ApplicationController
 
 
     private
+
+        def set_workout
+            @workout = Workout.find(params[:workout_id])
+        end
 
         def comment_params
             params.require(:comment).permit(:text)
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
-    validates :text, uniqueness: true
+    validates :text, presence: true
+    # validates :text, uniqueness: true
     belongs_to :user
     belongs_to :workout
 end
