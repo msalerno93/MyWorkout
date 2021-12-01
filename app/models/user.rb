441f80521387314319b6db16e3fd7d8ac2240657
RTBlanco <ronnytoribio1@hotmail.com>
2021-12-01 13:58:52 -0500

    added better validations for the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
     validates :username, presence: true
     has_many :workouts, :dependent => :delete_all
     
-    has_many :comments
+    has_many :comments, :dependent => :delete_all
     has_many :commented_workouts, through: :comments, source: :workout
 end
