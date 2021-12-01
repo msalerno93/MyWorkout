56873cb416d6ba208fa1479d8bb14b8c72afaa56
RTBlanco <ronnytoribio1@hotmail.com>
2021-12-01 13:58:30 -0500

    updated the comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,9 @@
 class CommentsController < ApplicationController
     before_action :set_workout
+    before_action :set_comment, only: [:edit, :update, :destroy]
 
-    def index
-    end
+    # def index
+    # end
 
     def new
         @comment = Comment.new
@@ -13,19 +14,30 @@ class CommentsController < ApplicationController
         if @comment.save
             redirect_to user_workout_path(@current_user, @workout)
         else
-            # redirect_to new_user_workout_comment_path(@current_user, @workout)
             @errors = @comment.errors.full_messages
             render :new
         end
     end
 
     def edit
+    
+    end
+     
+    def update 
+        if @comment.update(comment_params)
+            redirect_to user_workout_path(@current_user, @workout)
+        else
+            @errors = @comment.errors.full_messages
+            render :edit
+        end
     end
 
-    def show
-    end
+    # def show
+    # end
 
     def destroy
+        @comment.destroy
+        redirect_to user_workout_path(@current_user, @workout)
     end
 
 
@@ -34,6 +46,10 @@ class CommentsController < ApplicationController
 
         def set_workout
             @workout = Workout.find(params[:workout_id])
+        end
+
+        def set_comment 
+            @comment = Comment.find(params[:id])
         end
 
         def comment_params
