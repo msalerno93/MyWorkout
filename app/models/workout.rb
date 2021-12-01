5da48592ce4f815524b1a0d6d5950716fa8414fc
RTBlanco <ronnytoribio1@hotmail.com>
2021-12-01 13:59:25 -0500

    created scope method for the workout model

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,12 @@
+class MucsleValidator < ActiveModel::Validator
+    MUSCLE = ["chest", "back", "biceps", "triceps", "legs", "shoulders"]
+    def validate(record)
+        if !MUSCLE.include?(record.muscle.downcase)
+            record.errors.add :base, "must have on of these muscles (Chest, Back, Biceps, Triceps, Legs, or Shoulders)"
+        end
+    end
+end
+
 class Workout < ApplicationRecord
     # MUSCLE = ["Chest", "Back", "Biceps", "Triceps", "Legs", "Shoulders"]
     belongs_to :user
@@ -11,11 +20,11 @@ class Workout < ApplicationRecord
 
     scope :alpha, -> {order("workouts.title ASC")}
     # validate :muscle_is_valid?
-
+    validates_with MucsleValidator
 
     # private
     #     def muscle_is_valid?
-    #         if self.musclegroup && !MUSCLE.include?(self.musclegroup)
+    #         if !MUSCLE.include?(self.musclegroup)
     #             self.errors.add(:musclegroup, "Must use Muscles Already Provided")
     #         end
     #     end
